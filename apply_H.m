57027b8751a8720979183f8241d9a36ba10e3368
Xènia Salinas Ventalló <salinasxenia@gmail.com>
2017-12-16 09:27:09 +0100

    fix result image position

--- apply_H.m
+++ apply_H.m
@@ -18,16 +18,21 @@ ru2_e = [ru2_p(1)/ru2_p(3), ru2_p(2)/ru2_p(3)];
 ld2_e = [ld2_p(1)/ld2_p(3), ld2_p(2)/ld2_p(3)];
 rd2_e = [rd2_p(1)/rd2_p(3), rd2_p(2)/rd2_p(3)];
 
-m2 = ceil(abs(max([lu2_e(1), ru2_e(1), ld2_e(1), rd2_e(1)]) - min([lu2_e(1), ru2_e(1), ld2_e(1), rd2_e(1)])));
-n2 = ceil(abs(max([lu2_e(2), ru2_e(2), ld2_e(2), rd2_e(2)]) - min([lu2_e(2), ru2_e(2), ld2_e(2), rd2_e(2)])));
+m2 = ceil(abs(max([lu2_e(1), ru2_e(1), ld2_e(1), rd2_e(1)]) - min([lu2_e(1), ru2_e(1), ld2_e(1), rd2_e(1)]))) + 1;
+n2 = ceil(abs(max([lu2_e(2), ru2_e(2), ld2_e(2), rd2_e(2)]) - min([lu2_e(2), ru2_e(2), ld2_e(2), rd2_e(2)]))) + 1;
+
+%Compute the position rectification vector
+origin_m = min([lu2_e(1), ru2_e(1), ld2_e(1), rd2_e(1)]);
+origin_n = min([lu2_e(2), ru2_e(2), ld2_e(2), rd2_e(2)]);
 
 %Compute the homography transfromation
 I2 = zeros(m2,n2,c);
 
 for i = 1:m2
     for j = 1:n2
-        x_p = H\[i;j;1]; %A\b for inv(a)*b
-        x_e = round([x_p(1)/x_p(3), x_p(2)/x_p(3)]); %%TODO: Use interp2
+        x2_e = [i; j] + [origin_m; origin_n];
+        x2_p = H\[x2_e;1]; %A\b for inv(a)*b
+        x_e = round([x2_p(1)/x2_p(3), x2_p(2)/x2_p(3)]); %%TODO: Use interp2
         if (x_e(1) > 1 && x_e(2) > 1 && x_e(1) < m && x_e(2) < n)
             I2(i,j,:) = I(x_e(1),x_e(2),:);
         end
