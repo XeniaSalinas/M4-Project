33c2632a8fdde14ae3679f8ad423adb781dab74c
Santi Puch <santipuch590@gmail.com>
2017-12-19 19:48:55 +0100

    apply_H behaviour consisten with MATLAB implementation, imwarp

--- W1/apply_H.m
+++ W1/apply_H.m
@@ -4,54 +4,42 @@ function I2 = apply_H(I, H)
 %% Compute the size of the transformed image
 [n,m,c] = size(I);
 
-img_mask = (I(:,:,1)~=0 | I(:,:,2)~=0 | I(:,:,3)~=0);
+% Cartesian coordinates of the corners
+xmin = 1;
+xmax = m;
+ymin = 1;
+ymax = n;
 
-cor = corner(img_mask); % The default method used is Harris 
-% figure; imshow(I);
-% hold on;
-% plot(cor(:,1),cor(:,2),'r*');
-% hold off;
-
-%Cartesian coordinates of the image corners
-if isempty(cor)
-    lu_c = [1;1];
-    ru_c = [m;1];
-    ld_c = [1;n];
-    rd_c = [m;n];
-else
-    [~,min_n_idx] = min(cor(:,1));
-    [~,min_m_idx] = min(cor(:,2));
-    [~,max_n_idx] = max(cor(:,1));
-    [~,max_m_idx] = max(cor(:,2));
-    
-    lu_c = transpose(cor(min_n_idx,:));
-    ru_c = transpose(cor(min_m_idx,:));
-    ld_c = transpose(cor(max_n_idx,:));
-    rd_c = transpose(cor(max_m_idx,:));
-end
+lu_c = [xmin; ymin];  % Left-up
+ru_c = [xmax; ymin];  % Right-up
+ld_c = [xmin; ymax];  % Left-down
+rd_c = [xmax; ymax];  % Right down
 
 % Homogeneous coordinates of the corners
-lu_p = [lu_c;1];  % Left-up
-ru_p = [ru_c;1];  % Right-up
-ld_p = [ld_c;1];  % Left-down
-rd_p = [rd_c;1];  % Right down
+lu_p = [lu_c;1];  
+ru_p = [ru_c;1];  
+ld_p = [ld_c;1];  
+rd_p = [rd_c;1];  
+
 % Homogeneous coordinates of the transformed image's corners
 lu2_p = H * lu_p;  % Left-up 
 ru2_p = H * ru_p;  % Right-up
 ld2_p = H * ld_p;  % Left-down
 rd2_p = H * rd_p;  % Right down
+
 % Cartesian coordinates of the transformed image's corners
 lu2_c = [lu2_p(1)/lu2_p(3), lu2_p(2)/lu2_p(3)];
 ru2_c = [ru2_p(1)/ru2_p(3), ru2_p(2)/ru2_p(3)];
 ld2_c = [ld2_p(1)/ld2_p(3), ld2_p(2)/ld2_p(3)];
 rd2_c = [rd2_p(1)/rd2_p(3), rd2_p(2)/rd2_p(3)];
-% Rows (m2) and columns (n2) of the transformed image
-m2 = ceil(abs(max([lu2_c(1), ru2_c(1), ld2_c(1), rd2_c(1)]) - min([lu2_c(1), ru2_c(1), ld2_c(1), rd2_c(1)]))) + 1;
-n2 = ceil(abs(max([lu2_c(2), ru2_c(2), ld2_c(2), rd2_c(2)]) - min([lu2_c(2), ru2_c(2), ld2_c(2), rd2_c(2)]))) + 1;
+
+% Size of the transformed image
+m2 = ceil(abs(max([lu2_c(1), ru2_c(1), ld2_c(1), rd2_c(1)]) - min([lu2_c(1), ru2_c(1), ld2_c(1), rd2_c(1)]))) + 1; % columns
+n2 = ceil(abs(max([lu2_c(2), ru2_c(2), ld2_c(2), rd2_c(2)]) - min([lu2_c(2), ru2_c(2), ld2_c(2), rd2_c(2)]))) + 1; % rows
 
 %% Compute the position rectification vector
-origin_m = round(min([lu2_c(1), ru2_c(1), ld2_c(1), rd2_c(1)]));
-origin_n = round(min([lu2_c(2), ru2_c(2), ld2_c(2), rd2_c(2)]));
+xoffset = floor(min([lu2_c(1), ru2_c(1), ld2_c(1), rd2_c(1)]));
+yoffset = floor(min([lu2_c(2), ru2_c(2), ld2_c(2), rd2_c(2)]));
 
 %% Compute the homography transformation
 I2 = zeros(n2,m2,c);
@@ -62,12 +50,18 @@ x2_aux_2 = zeros(n2,m2);
 
 for i = 1:n2
     for j = 1:m2
-        x2_e = [j; i] + [origin_m; origin_n];
+        % Alias for better understanding
+        x_idx = j;
+        y_idx = i;
+        
+        % Cartesian coordinate of the transformed image to be obtained in
+        % the original image
+        x2_e = [x_idx; y_idx] + [xoffset; yoffset];
         x2_p = H \ [x2_e; 1]; % same as inv(H)*b, but more accurate and efficient
         x_e = [x2_p(1)/x2_p(3), x2_p(2)/x2_p(3)];
         % If the transformed point is inside the source image, save the
         % point coordinates
-        if (x_e(1) >= 1 && x_e(2) >= 1 && x_e(1) <= n && x_e(2) <= m)
+        if (x_e(1) >= 1 && x_e(2) >= 1 && x_e(1) <= m && x_e(2) <= n)
             indices(i,j) = 1;
             x2_aux_1(i,j) = x_e(1);
             x2_aux_2(i,j) = x_e(2);
