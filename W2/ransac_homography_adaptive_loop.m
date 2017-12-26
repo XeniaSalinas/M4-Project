3b7aecf534ff849540618d87f16effdc337c9bc1
Anna Martí <annamartiaguilera@gmail.com>
2017-12-26 20:28:25 +0100

    added computation of the symmetric geometric error

--- W2/ransac_homography_adaptive_loop.m
+++ W2/ransac_homography_adaptive_loop.m
@@ -31,27 +31,38 @@ end
 % compute H from all the inliers
 H = homography2d(x1(:,best_inliers), x2(:,best_inliers));
 idx_inliers = best_inliers;
-
+end
 
 function idx_inliers = compute_inliers(H, x1, x2, th)
-    % Check that H is invertible
-    if abs(log(cond(H))) > 15
-        idx_inliers = [];
-        return
-    end
-    
+   % Check that H is invertible
+   if abs(log(cond(H))) > 15
+       idx_inliers = [];
+       return
+   end    
+   % compute the symmetric geometric error d2
+   s=size(x1);
+   d2=0;
+   for i=1:s(2)
+       x2_=x2(:,i)/x2(3,i);
+       x1_=x1(:,i)/x1(3,i);
+       x1_trans=H*x1_;
+       x1_trans=x1_trans/x1_trans(3,:);
+       x2_trans=(H)\x2_;
+       x2_trans=x2_trans/x2_trans(3,:);
 
-    % compute the symmetric geometric error
-    d2 = % ToDo
-    idx_inliers = find(d2 < th.^2);
-
+       a = cross(x1_trans,x2_);
+       b = cross(x2_trans,x1_);
+       d2(i)= sum(a(1,:).^2+a(2,:).^2) + sum(b(1,:).^2+b(2,:).^2);
+   end
+   idx_inliers = find(d2 < th.^2);
+end
 
 function xn = normalise(x)    
     xn = x ./ repmat(x(end,:), size(x,1), 1);
 
-    
+end   
 function item = randomsample(npts, n)
-	a = [1:npts]; 
+	a = 1:npts; 
     item = zeros(1,n);    
     for i = 1:n
 	  % Generate random value in the appropriate range 
@@ -59,3 +70,4 @@ function item = randomsample(npts, n)
 	  item(i) = a(r);       % Select the rth element from the list
 	  a(r)    = a(end-i+1); % Overwrite selected element
     end                       % ... and repeat
+end
